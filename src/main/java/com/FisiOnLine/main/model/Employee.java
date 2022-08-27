357136296d800c9603383981c698d8b6eb6c8b27
JanetMU <janet_marulanda@yahoo.com>
2022-08-27 11:45:52 -0500

    Creacion clase Employee

---
+++ src/main/java/com/FisiOnLine/main/model/Employee.java
@@ -0,0 +1,92 @@
+package com.FisiOnLine.main.model;
+import java.util.Date;
+
+public class Employee {
+    //Atributos
+    private long id;
+    private String email;
+    private Profile profile;
+    private Enum_RoleName role;
+    private Enterprise enterprise;
+    private Transaction [] transactions;
+    private Date updatedAt;
+    private Date cratedAt;
+
+    //Constructor
+    public Employee(long id, String email, Profile profile, Enum_RoleName role, Enterprise enterprise) {
+        this.id = id;
+        this.email = email;
+        this.profile = profile;
+        this.role = role;
+        this.enterprise = enterprise;
+    }
+    //Constructor sin Atributos
+
+    public Employee() {
+    }
+    //Getters y Setters
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Profile getProfile() {
+        return profile;
+    }
+
+    public void setProfile(Profile profile) {
+        this.profile = profile;
+    }
+
+    public Enum_RoleName getRole() {
+        return role;
+    }
+
+    public void setRole(Enum_RoleName role) {
+        this.role = role;
+    }
+
+    public Enterprise getEnterprise() {
+        return enterprise;
+    }
+
+    public void setEnterprise(Enterprise enterprise) {
+        this.enterprise = enterprise;
+    }
+
+    public Transaction[] getTransactions() {
+        return transactions;
+    }
+
+    public void setTransactions(Transaction[] transactions) {
+        this.transactions = transactions;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Date getCratedAt() {
+        return cratedAt;
+    }
+
+    public void setCratedAt(Date cratedAt) {
+        this.cratedAt = cratedAt;
+    }
+}
