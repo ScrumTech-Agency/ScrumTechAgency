db60293dc89fdadd449e04e6526cf3da75d4c7a3
Andres Felipe Mena Guacas <andres.mena@ucc.edu.co>
2022-09-25 08:39:19 -0500

    Template add transaction y wellcome transaction OK

--- src/main/java/com/FisiOnLine/main/controller/FrontControllers.java
+++ src/main/java/com/FisiOnLine/main/controller/FrontControllers.java
@@ -1,6 +1,8 @@
 package com.FisiOnLine.main.controller;
 
 import com.FisiOnLine.main.model.Transaction;
+import com.FisiOnLine.main.service.TransactionManagerInterface;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,6 +23,9 @@ public class FrontControllers {
 
     //Controladores Transaction
 
+    @Autowired
+    private TransactionManagerInterface gestorTransaction;
+
     @GetMapping("/addtransaction")
     public String getAddTransaction (Model model){
         model.addAttribute("formTransaction", new Transaction());
@@ -29,6 +34,7 @@ public class FrontControllers {
 
     @GetMapping("/wellcometransaction")
     public String getWellcomeTransaction (Model model){
+        model.addAttribute("transactions",gestorTransaction.getTransactions());
         return "wellcome-transaction-new";
     }
 
